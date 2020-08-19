4e5938b8a6f5b2a62bd4790e5daec3c0c8041ce3
Rohan Sawant <rohansawantct83@gmail.com>
2020-08-19 21:29:32 +0530

    proposed model

--- api/models/search_results.py
+++ api/models/search_results.py
@@ -1,15 +1,38 @@
 from flask import current_app
-from pynamodb.attributes import NumberAttribute, UnicodeAttribute
+from pynamodb.attributes import NumberAttribute, UnicodeAttribute, MapAttribute
 from pynamodb.models import Model
 
+class BaseModel(Model):
 
-class User(Model):
+
+class SearchEntries(MapAttribute):
     class Meta:
-        table_name = 'User'
+        table_name = 'SearchEntries'
+        host = current_app.config['DB_URL']
+
+    name = UnicodeAttribute(null=False)
+    url = UnicodeAttribute(null=False)
+
+
+class SearchResults(MapAttribute):
+    class Meta:
+        table_name = 'SearchResults'
+        host = current_app.config['DB_URL']
+
+    make = UnicodeAttribute(null=False)
+    model = UnicodeAttribute(null=True)
+
+
+"""Every search_request -> several search results, -> several search entries"""
+
+
+class SearchRequests(Model):
+    class Meta:
+        table_name = 'SearchRequests'
         host = current_app.config['DB_URL']
 
     id = NumberAttribute(hash_key=True)
     name = UnicodeAttribute()
 
     def __repr__(self):
-        return "<User {} name:{}>".format(self.id, self.name)
+        return "<SearchResults {} name:{}>".format(self.id, self.name)
