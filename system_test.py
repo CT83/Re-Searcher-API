878550c65b3d703009060df7c693221b421858d3
Rohan Sawant <rohansawantct83@gmail.com>
2020-08-20 13:31:42 +0530

    Added system test

--- system_test.py
+++ system_test.py
@@ -1,9 +1,24 @@
-print("Searching new terms...")
-print("Time Taken:")
-#
+import random
+import time
 
+import requests
+
+print("Setting things up...")
+url = "http://localhost/api/search"
+requests.post(url, json={"query": "Pink Car", "engine": "bing"})
 
 print("Searching existing terms...")
-print("Time Taken:")
+for attempt in range(5):
+    start = time.time()
+    payload = {"query": "Pink Car", "engine": "bing"}
+    res = requests.post(url, json=payload)
+    assert res.status_code == 200
+    print("Existing | {} Time Taken :{}".format(attempt, time.time() - start))
 
-#
+print("Searching new terms...")
+for attempt in range(5):
+    start = time.time()
+    payload = {"query": str(random.randint(0, 9999)), "engine": "bing"}
+    res = requests.post(url, json=payload)
+    assert res.status_code == 200
+    print("Existing | {} Time Taken :{}".format(attempt, time.time() - start))
